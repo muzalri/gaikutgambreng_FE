ca46ffd426053a11f437bb08a4b29246204d125a
muzalri <88919404+muzalri@users.noreply.github.com>
2025-11-24 22:13:22 +0700

    perbaikan cors google

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || '58439630123-7ae8hjdo1o5pdofdim5qcou82oic1rjm.apps.googleusercontent.com';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
